4d97f188e6e6efa4778520190f78b9dc6e7433fb
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-09-10 20:34:16 -0400

    created a print function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import models.Note;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Note> noteBook = new ArrayList<>();
+        //creating a note and adding it to noteBook ArrayList
+
+        Note note1 = new Note( 1, "Note 1", "This is my very first note");
+        noteBook.add(note1);
+        noteBook.add(new Note( 2, "Note 2", "This is my second note"));
+        noteBook.add(new Note( 3, "Note 3", "This is my third note"));
+//Printing noteslist
+        for (Note note : noteBook) {
+            note.print();
+
+        }
+
     }
+
+
+
 }
--- src/models/Note.java
+++ src/models/Note.java
@@ -1,4 +1,45 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Note {
+    private int id;
+    private String title;
+    private String content;
+    private ArrayList<Note> noteBook = new ArrayList<>();
+
+
+
+
+
+    public Note(int id, String title, String content) {
+        this.id =id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void createNote(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter id:");
+        int id = sc.nextInt();
+
+        System.out.println("Enter title:");
+        String title = sc.nextLine();
+
+        System.out.println("Enter content:");
+        String content = sc.nextLine();
+        Note note = new Note(id, title, content);
+
+    }
+    public void print(){
+        String note = "- ".repeat(15);
+        note += "\n Note: ";
+        note += this.id;
+        note += "\n Title: " + this.title;
+        note += "\n Content: " + this.content+"\n";
+        System.out.println(note);
+
+//        System.out.println("id: "+id+ " title: " +title+" content: "+content);
+    }
 }
